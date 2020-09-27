f193ffc646ae4b5e90d6c03d1f7e12654156ffe5
Yigang Zhou <zhou@zygmail.com>
2020-09-28 01:00:10 +0800

    9.28

--- countServers.py
+++ countServers.py
@@ -6,15 +6,39 @@
 # 1267. 统计参与通信的服务器
 # https://leetcode-cn.com/problems/count-servers-that-communicate/
 from typing import List
+import common.arrayCommon as Array
 
 
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         h = len(grid)
         w = len(grid[0])
+        count = 0
+        for i in range(h):
+            for j in range(w):
+                if grid[i][j] == 1:
+                    c = self.dfs(i, j, w, h, grid)
+                    if c != 1:
+                        count += c
+        return count
 
-        
+    def dfs(self, i, j, w, h, grid):
+        count = 0
+        for y in range(h):
+            if grid[y][j] == 1:
+                grid[y][j] = 2
+                count += 1 + self.dfs(y, j, w, h, grid)
+        for x in range(w):
+            if grid[i][x] == 1:
+                grid[i][x] = 2
+                count += 1 + self.dfs(i, x, w, h, grid)
+        return count
 
 
-grid = [[1, 1, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+grid = [[0,0,0,1,0,0,0,0,0,0],[1,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,1,0,0]]
+Array.print2DArray(grid)
 r = Solution().countServers(grid)
+print()
+Array.print2DArray(grid)
+
+print(r)
